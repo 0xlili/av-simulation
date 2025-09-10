6f3b2ef7e00f334983d7da35f567edd9bd1b9f22
Lina Nada Maach <lin.louni@gmail.com>
2025-09-10 08:40:53 -0700

    Update overtaking_cyclist_bidirectional_road.py

--- main/scenarios/overtaking_cyclist_bidirectional_road.py
+++ main/scenarios/overtaking_cyclist_bidirectional_road.py
@@ -40,7 +40,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def main(weightofpolicy, weightofdriver, weightofcyclist, ideal_weight_cyclist, ideal_weight_driver, ideal_weight_policymaker, xxx1, xxx2,, yyy, zzz, replanner: bool = False, vis_frame: bool = False, save_weight_table: bool = False) -> None:
+def main(weightofpolicy, weightofdriver, weightofcyclist, ideal_weight_cyclist, ideal_weight_driver, ideal_weight_policymaker, xxx1, xxx2, xxx3, yyy, zzz, replanner: bool = False, vis_frame: bool = False, save_weight_table: bool = False) -> None:
     """
     Main function to simulate the scenario of an AV overtaking a cyclist in a bidirectional road.
 
