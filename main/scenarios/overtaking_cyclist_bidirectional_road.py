b503dc5b0eef701feaa52388c7bdde3862bcf681
Lina Nada Maach <lin.louni@gmail.com>
2025-09-11 02:38:00 -0700

    Update overtaking_cyclist_bidirectional_road.py

--- main/scenarios/overtaking_cyclist_bidirectional_road.py
+++ main/scenarios/overtaking_cyclist_bidirectional_road.py
@@ -2650,7 +2650,7 @@ d_trajectory_evaluation_spatial = d_results_folder / "d_trajectory_evaluation_sp
 d_trajectory_evaluations_trajectories = d_results_folder / "d_trajectory_evaluation_trajectries.png"
 
 video_path = "d_output_video.mp4"
-spatial_image_path = "d_trajectory_evaluation_spatial.png"
+spatial_image_path = "d_trajectory_evaluations_spatial.png"
 trajectory_image_path = "d_trajectory_evaluations_trajectories.png"
 
 # --- Default Outputs ---
