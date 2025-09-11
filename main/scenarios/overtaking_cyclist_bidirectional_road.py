32e4207bc196782a65e71278e92c235d54724314
Lina Nada Maach <lin.louni@gmail.com>
2025-09-11 02:44:52 -0700

    Update overtaking_cyclist_bidirectional_road.py

--- main/scenarios/overtaking_cyclist_bidirectional_road.py
+++ main/scenarios/overtaking_cyclist_bidirectional_road.py
@@ -2646,8 +2646,8 @@ grandparent_dir = script_dir.parent.parent
 d_results_folder = grandparent_dir / "results" 
 d_output_video_path = d_results_folder / "d_output_video.mp4"
 print(d_output_video_path)
-d_trajectory_evaluation_spatial = d_results_folder / "d_trajectory_evaluation_spatial.png"
-d_trajectory_evaluations_trajectories = d_results_folder / "d_trajectory_evaluation_trajectries.png"
+d_trajectory_evaluation_spatial = d_results_folder / "d_trajectory_evaluations_spatial.png"
+d_trajectory_evaluations_trajectories = d_results_folder / "d_trajectory_evaluations_trajectries.png"
 
 
 video_path = os.path.join(script_dir, "d_output_video.mp4")
