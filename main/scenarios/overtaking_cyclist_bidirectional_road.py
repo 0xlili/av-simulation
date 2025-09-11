0ea30f8e910c5edb8b53b8ece9995e60ce1ca839
Lina Nada Maach <lin.louni@gmail.com>
2025-09-11 02:43:51 -0700

    Update overtaking_cyclist_bidirectional_road.py

--- main/scenarios/overtaking_cyclist_bidirectional_road.py
+++ main/scenarios/overtaking_cyclist_bidirectional_road.py
@@ -2649,14 +2649,15 @@ print(d_output_video_path)
 d_trajectory_evaluation_spatial = d_results_folder / "d_trajectory_evaluation_spatial.png"
 d_trajectory_evaluations_trajectories = d_results_folder / "d_trajectory_evaluation_trajectries.png"
 
-video_path = "d_output_video.mp4"
-spatial_image_path = "d_trajectory_evaluations_spatial.png"
-trajectory_image_path = "d_trajectory_evaluations_trajectories.png"
+
+video_path = os.path.join(script_dir, "d_output_video.mp4")
+spatial_image_path = os.path.join(script_dir, "d_trajectory_evaluation_spatial.png")
+trajectory_image_path = os.path.join(script_dir, "d_trajectory_evaluations_trajectories.png")
 
 # --- Default Outputs ---
-st.video(video_path)
-st.image(spatial_image_path, caption="Spatial Trajectory Evaluation")
-st.image(trajectory_image_path, caption="Trajectory Evaluation")
+st.video(str(video_path))
+st.image(str(spatial_image_path), caption="Spatial Trajectory Evaluation")
+st.image(str(trajectory_image_path), caption="Trajectory Evaluation")
 
 # --- 1st weight set ---
 with st.container():
