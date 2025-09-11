876b9d6a07dc36b6cc10f79b90fb84c6b5ff6873
Lina Nada Maach <lin.louni@gmail.com>
2025-09-11 02:51:25 -0700

    Update overtaking_cyclist_bidirectional_road.py

--- main/scenarios/overtaking_cyclist_bidirectional_road.py
+++ main/scenarios/overtaking_cyclist_bidirectional_road.py
@@ -2613,8 +2613,8 @@ def run_simulation(weightofpolicy, weightofdriver, weightofcyclist, ideal_weight
             # Generate output video using ffmpeg
             input_images_path = results_folder / "frame_%04d.jpg"
             output_video_path = results_folder / "output_video.mp4"
-            trajectory_evaluation_spatial = results_folder / "trajectory_evaluation_spatial.png"
-            trajectory_evaluations_trajectories = results_folder / "trajectory_evaluation_trajectries.png"
+            trajectory_evaluation_spatial = results_folder / "trajectory_evaluations_spatial.png"
+            trajectory_evaluations_trajectories = results_folder / "trajectory_evaluations_trajectries.png"
             
 
             subprocess.run([
