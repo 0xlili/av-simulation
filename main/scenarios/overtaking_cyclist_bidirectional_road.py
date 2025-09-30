6ed4410f6bec102a992d49b4383d594a70df8212
Lina Nada Maach <lin.louni@gmail.com>
2025-09-30 18:52:21 +0200

    Update overtaking_cyclist_bidirectional_road.py

--- main/scenarios/overtaking_cyclist_bidirectional_road.py
+++ main/scenarios/overtaking_cyclist_bidirectional_road.py
@@ -349,7 +349,18 @@ def perform_replan(weightofpolicy, weightofdriver, weightofcyclist, ideal_weight
 
     # Add the trajectory to the list of trajectories to the last position
     trajectories_full.append((follow_trajectory,(0.0, 0.0, 0.0, 0.0, 0.0)))
-    print(trajectories_full)
+    print("YOYOYOYOYOYO", trajectories_full)
+    print('''
+
+
+
+
+
+
+
+
+
+    ''')
 
     if save_weight_table == True:
         # Evaluate trajectories based on human-centered reasons
